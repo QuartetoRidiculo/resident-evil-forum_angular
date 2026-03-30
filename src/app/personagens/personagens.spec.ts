921c36e37b8f16e1c9cc12cc23e31da57d444533
Darllancabral <cabraldarllan@gmail.com>
2026-03-30 15:16:03 -0300

    i1.0

---
+++ src/app/personagens/personagens.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Personagens } from './personagens';
+
+describe('Personagens', () => {
+  let component: Personagens;
+  let fixture: ComponentFixture<Personagens>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Personagens],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Personagens);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/personagens/personagens.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personagens',
+  imports: [],
+  templateUrl: './personagens.html',
+  styleUrl: './personagens.css',
+})
+export class Personagens {}
