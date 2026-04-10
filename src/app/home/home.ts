f9e66fbe824137290119ce7ee11cc18b7b0e5a4a
Darllancabral <cabraldarllan@gmail.com>
2026-04-10 18:56:59 -0300

    login

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
